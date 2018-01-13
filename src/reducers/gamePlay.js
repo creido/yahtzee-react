caee8c6d353f70a529e862d17336727b995086bc
Chris Reid <chris.reid@akqa.com>
2018-01-13 01:05:51 +0000

    Changed player ids to start at 0 for easier array indexing

--- src/reducers/gamePlay.js
+++ src/reducers/gamePlay.js
@@ -3,20 +3,20 @@ import {showMessage} from './messages';
 
 const initialState = {
   players: [
-    {id: 1, name: 'Player 1', isActive: true},
-    {id: 2, name: 'Player 2', isActive: false},
-    {id: 3, name: 'Player 3', isActive: false},
-    {id: 4, name: 'Player 4', isActive: false},
+    {id: 0, name: 'Player 1', isActive: true},
+    {id: 1, name: 'Player 2', isActive: false},
+    {id: 2, name: 'Player 3', isActive: false},
+    {id: 3, name: 'Player 4', isActive: false},
   ],
-  activePlayer: 1,
+  activePlayer: 0,
   roll: 0,
   canRoll: true,
 };
 
 const MAX_ROLLS = 3;
 
-export const SET_NEXT_PLAYER = 'SET_NEXT_PLAYER';
-export const SET_ROLL = 'SET_ROLL';
+const SET_NEXT_PLAYER = 'SET_NEXT_PLAYER';
+const SET_ROLL = 'SET_ROLL';
 
 const actionSetNextPlayer = id => ({type: SET_NEXT_PLAYER});
 
@@ -31,23 +31,24 @@ export const setNextPlayer = id => {
 export default (state = initialState, action) => {
 
   switch (action.type) {
+
     case SET_NEXT_PLAYER:
       // TODO: move this logic into separate function
-      const nextPlayer = state.activePlayer === state.players.length
-        ? 1
+      const nextPlayer = state.activePlayer === state.players.length - 1
+        ? 0
         : state.activePlayer + 1;
 
       return {
-          ...state,
-          activePlayer: nextPlayer,
-          roll: 0,
-          canRoll: true,
-          players: state.players.map(p => {
-            return p.id === nextPlayer
-              ? {...p, isActive: true}
-              : {...p, isActive: false}
-            })
-        };
+        ...state,
+        activePlayer: nextPlayer,
+        roll: 0,
+        canRoll: true,
+        players: state.players.map(p => {
+          return p.id === nextPlayer
+            ? {...p, isActive: true}
+            : {...p, isActive: false}
+          })
+      };
 
     case SET_ROLL:
       // TODO: move this logic into separate function
